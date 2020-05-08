b7ab08e210872585d6f38fcd6725bb9563fbd344
deshario <deshario9@gmail.com>
2020-05-08 16:02:40 +0700

    Day11

---
+++ 11 - Text Reveal/start.js
@@ -0,0 +1,27 @@
+(() => {
+  // เริ่มเขียนโค้ด
+
+
+  function onScroll(){
+    const sectionElements = Array.from(document.querySelectorAll('section'));
+    sectionElements.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+      const scrollPosition = window.pageYOffset;
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+      // imgElem.offsetTop = image top position  
+      // imgElem.offsetHeight = imageHeight
+      if(scrollPosition >= revealPosition){
+        textElem.classList.add('reveal');
+      }
+    })
+  }
+
+  function run(){
+    document.addEventListener('scroll',onScroll);
+  }
+
+
+  run();
+
+})();
